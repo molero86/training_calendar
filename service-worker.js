48ccf068d358a6da96e59a4c5a50a559fab1047e
molero86 <molero_30@hotmail.com>
2025-01-29 10:02:28 +0100

    Service worker updated to manage updates on installed apps

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,19 @@
+const CACHE_NAME = 'training-app-cache-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/css/styles.css',
+    '/js/scripts.js',
+    '/icons/icon-192x192.png',
+    '/icons/icon-256x256.png',
+    '/icons/icon-384x384.png',
+    '/icons/icon-512x512.png'
+];
+
 self.addEventListener('install', function(event) {
     event.waitUntil(
-        caches.open('training-app-cache').then(function(cache) {
-            return cache.addAll([
-                '/',
-                '/index.html',
-                '/css/styles.css',
-                '/js/scripts.js',
-                '/icons/icon-192x192.png',
-                '/icons/icon-256x256.png',
-                '/icons/icon-384x384.png',
-                '/icons/icon-512x512.png'
-            ]);
+        caches.open(CACHE_NAME).then(function(cache) {
+            return cache.addAll(urlsToCache);
         })
     );
 });
@@ -19,6 +22,21 @@ self.addEventListener('fetch', function(event) {
     event.respondWith(
         caches.match(event.request).then(function(response) {
             return response || fetch(event.request);
+        })
+    );
+});
+
+self.addEventListener('activate', function(event) {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.map(function(cacheName) {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
         })
     );
 });
